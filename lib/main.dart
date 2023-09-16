4c3298f9d513a4e2bf33b26f40589c8ba58b9dcb
Tejaswi Khandavalli <kvrtejaswi@gmail.com>
2023-09-16 00:14:31 -0500

    Configured font theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,17 @@
 import 'package:flutter/material.dart';
+import 'Authentication/login.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      home: LoginApp(), // Replace with your app's initial screen
     );
   }
 }
+
+
